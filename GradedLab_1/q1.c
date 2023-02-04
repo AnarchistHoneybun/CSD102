c3b5866a2f19b222c3f62d941b5850f3b7d66ea4
AnarchistHoneybun <74085528+AnarchistHoneybun@users.noreply.github.com>
2023-02-05 05:21:14 +0530

    Update q1.c

--- GradedLab_1/q1.c
+++ GradedLab_1/q1.c
@@ -1,3 +1,25 @@
+/*Q: Write a C program to multiply two matrices of size NxN.
+
+Input:
+Size N
+N*N elements from Matrix 1
+N*N elements from Matrix 2
+
+Output:
+N*N elements from (Matrix 1 x Matrix 2)
+
+Example:
+
+Input:
+3
+1 2 3 4 5 6 7 8 9
+4 5 6 7 8 9 0 1 2
+
+Output:
+18 24 30 51 66 81 84 108 132
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
